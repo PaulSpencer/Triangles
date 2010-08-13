ab78873f1368421f093e3fee2fcc5c1590fa9fe5
unknown <Vlad@.(none)>
2010-08-13 22:41:30 +0200

    Trying to commit a factory stub

---
+++ TriangleAssignment/TriangleTestSuite/GeometryShapeFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeometricalObjects
+{
+    public static class GeometryShapeFactory
+    {
+        public static Triangle CreateTriangle(Point point1, Point point2, Point point3, IGeometryCalculator calculator)
+        {
+            return null;
+        }
+    }
+}
